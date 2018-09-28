30e27e783ca590a0bfdc28bb35d80f3e64eb1d49
chehanr <chehan.rat@gmail.com>
2018-09-28 13:33:40 +1000

    Added GZip middleware.

--- tutreq/settings.py
+++ tutreq/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'tutreq.urls'
