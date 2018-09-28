93ea710b0b6f7f0adcdae831db201051d73e380d
chehanr <chehan.rat@gmail.com>
2018-09-28 14:10:50 +1000

    Removed unused import.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,4 @@
-from django.core.validators import (MaxValueValidator, RegexValidator,
-                                    ValidationError)
+from django.core.validators import MaxValueValidator, RegexValidator
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
